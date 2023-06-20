92cba2e8c792ddaf09cbd01ba3c8a17f32ae06bf
arungit2020 <aruprajapati9825@gmail.com>
2023-06-20 20:50:17 +0530

    redux toolkit setup done

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,19 @@ import { ApolloProvider } from "@apollo/client";
 import client from "./apollo/client.ts";
 import { BrowserRouter } from "react-router-dom";
 import UserProvider from "./auth/AuthProvider.tsx";
+import { store } from "./store/store.ts";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<>
-	<UserProvider>
-		<BrowserRouter>
-			<ApolloProvider client={client}>
-				<App />
-			</ApolloProvider>
-		</BrowserRouter>
-	</UserProvider>
+		<UserProvider>
+			<BrowserRouter>
+				<Provider store={store}>
+					<ApolloProvider client={client}>
+						<App />
+					</ApolloProvider>
+				</Provider>
+			</BrowserRouter>
+		</UserProvider>
 	</>
 );
